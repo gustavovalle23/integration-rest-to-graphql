05b2d49f0894c956e75ab5bdebc34f15db77b4ac
Gustavo Valle <gustavo.vallerp@hotmail.com>
2022-03-04 12:11:39 -0300

    add initial search products from json

--- schema.py
+++ schema.py
@@ -2,14 +2,16 @@ from typing import List
 import strawberry
 
 from definitions.company import Company
+from definitions.product_search import ProductSearch
 from definitions.store import Store
 from rest.company import get_data_company_from_rest
+from rest.product_search import get_data_product_search_from_rest
 from rest.store import get_data_loja_from_rest
 
 
 @strawberry.type
 class Query:
-    
+
     @strawberry.field
     def dados_empresa() -> Company:
         return get_data_company_from_rest()
@@ -17,3 +19,7 @@ class Query:
     @strawberry.field
     def dados_loja(id: int) -> Store:
         return get_data_loja_from_rest(id)
+
+    @strawberry.field
+    def listing_of_search() -> ProductSearch:
+        return get_data_product_search_from_rest()
