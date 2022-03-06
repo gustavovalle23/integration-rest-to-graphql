dca27c086487f6c8f84a386e130b8c25faa5ac02
Gustavo Valle <gustavo.vallerp@hotmail.com>
2022-03-05 22:19:24 -0300

    Run server with python command

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import strawberry
 
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
+import uvicorn
 from schema import Query
 
 
@@ -14,3 +15,7 @@ graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
