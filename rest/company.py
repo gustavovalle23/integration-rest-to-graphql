f0c8e61dfc73f859e77d82604ffcc060dc851fa6
Gustavo Valle <gustavo.vallerp@hotmail.com>
2022-03-05 22:39:34 -0300

    removing useless declarations

--- rest/company.py
+++ rest/company.py
@@ -1,4 +1,3 @@
-import json
 import requests
 
 from definitions.company import Company, FacebookLogin, GoogleLogin
@@ -8,45 +7,45 @@ def _request() -> dict:
     return requests.get('https://sandbox.carrinhocerto.com.br/api/dados-empresa/v2/').json()
 
 
-def get_data_company_from_rest():
+def get_data_company_from_rest() -> Company:
     data = _request()
 
     return Company(
-        logo=data['logo'],
-        logo_mobile=data['logo_mobile'],
-        dominio=data['dominio'],
-        endereco_facebook=data['endereco_facebook'],
-        endereco_twitter=data['endereco_twitter'],
-        endereco_instagram=data['endereco_instagram'],
-        endereco_youtube=data['endereco_youtube'],
-        email_contato=data['email_contato'],
-        ramo=data['ramo'],
-        possui_politica_privacidade=data['possui_politica_privacidade'],
-        qtd_lojas_ativas=data['qtd_lojas_ativas'],
-        onesignal_app_id=data['onesignal_app_id'],
-        build_minimo_app=data['build_minimo_app'],
-        key_api_google=data['key_api_google'],
-        facebook_login=FacebookLogin(
+        data['logo'],
+        data['logo_mobile'],
+        data['dominio'],
+        data['endereco_facebook'],
+        data['endereco_twitter'],
+        data['endereco_instagram'],
+        data['endereco_youtube'],
+        data['email_contato'],
+        data['ramo'],
+        data['possui_politica_privacidade'],
+        data['qtd_lojas_ativas'],
+        data['onesignal_app_id'],
+        data['build_minimo_app'],
+        data['key_api_google'],
+        FacebookLogin(
             client_id=data['facebook_login'].get('client_id', ''),
             secret=data['facebook_login'].get('secret', '')
         ),
-        google_login=GoogleLogin(
-            client_id=data['google_login'].get('client_id', ''),
-            secret=data['google_login'].get('secret', '')
+        GoogleLogin(
+            data['google_login'].get('client_id', ''),
+            data['google_login'].get('secret', '')
         ),
-        loja_com_geolocalizacao=data['loja_com_geolocalizacao'],
-        tags_observacao_produto=data['tags_observacao_produto'],
-        data_nascimento_obrigatoria=data['data_nascimento_obrigatoria'],
-        telefone_cadastro_inicial=data['telefone_cadastro_inicial'],
-        imagem_bg_home=data['imagem_bg_home'],
-        utilizar_google_places=data['utilizar_google_places'],
-        script_analytics=data['script_analytics'],
-        script_google_sent_to=data['script_google_sent_to'],
-        script_chat=data['script_chat'],
-        script_tag_manager_head=data['script_tag_manager_head'],
-        script_tag_manager_body=data['script_tag_manager_body'],
-        script_pixel_facebook=data['script_pixel_facebook'],
-        script_google_merchant=data['script_google_merchant'],
-        key_api_google_site=data['key_api_google_site'],
-        filtro_por_geolocalizacao=data['filtro_por_geolocalizacao'],
+        data['loja_com_geolocalizacao'],
+        data['tags_observacao_produto'],
+        data['data_nascimento_obrigatoria'],
+        data['telefone_cadastro_inicial'],
+        data['imagem_bg_home'],
+        data['utilizar_google_places'],
+        data['script_analytics'],
+        data['script_google_sent_to'],
+        data['script_chat'],
+        data['script_tag_manager_head'],
+        data['script_tag_manager_body'],
+        data['script_pixel_facebook'],
+        data['script_google_merchant'],
+        data['key_api_google_site'],
+        data['filtro_por_geolocalizacao'],
     )
